1edfa7ba156b14fdb109358c39137fc4d7d2a08f
tvarkevi <61967027+tvarkevi@users.noreply.github.com>
2020-03-11 10:25:24 +0100

    Add files via upload

---
+++ Smoothing_job.m
@@ -0,0 +1,10 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-Feb-2020 15:15:22 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.smooth.data = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.smooth.fwhm = [8 8 8];
+matlabbatch{1}.spm.spatial.smooth.dtype = 0;
+matlabbatch{1}.spm.spatial.smooth.im = 0;
+matlabbatch{1}.spm.spatial.smooth.prefix = 's';
