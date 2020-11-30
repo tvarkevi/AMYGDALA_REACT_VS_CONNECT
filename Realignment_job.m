c93f56a327651b9e703b38842c68a50efee84a41
Tim Varkevisser <61967027+tvarkevi@users.noreply.github.com>
2020-11-30 10:40:38 +0100

    Update Realignment_job.m

--- Realignment_job.m
+++ Realignment_job.m
@@ -1,1 +1,19 @@
-
+%-----------------------------------------------------------------------
+% Job saved on 06-Jul-2020 09:39:38 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.realign.estimate.data = {'<UNDEFINED>'};
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.fwhm = 5;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.rtm = 1;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.interp = 2;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.weight = '';
+matlabbatch{2}.spm.spatial.realign.write.data = {'<UNDEFINED>'};
+matlabbatch{2}.spm.spatial.realign.write.roptions.which = [2 1];
+matlabbatch{2}.spm.spatial.realign.write.roptions.interp = 4;
+matlabbatch{2}.spm.spatial.realign.write.roptions.wrap = [0 0 0];
+matlabbatch{2}.spm.spatial.realign.write.roptions.mask = 1;
+matlabbatch{2}.spm.spatial.realign.write.roptions.prefix = 'r';
