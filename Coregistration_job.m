84ca7e25be2a9bf5a966fe6dc9986e989a34730c
Tim Varkevisser <61967027+tvarkevi@users.noreply.github.com>
2020-11-30 10:28:10 +0100

    Update Coregistration_job.m

--- Coregistration_job.m
+++ Coregistration_job.m
@@ -1,16 +1,12 @@
 %-----------------------------------------------------------------------
-% Job saved on 07-Feb-2020 16:18:28 by cfg_util (rev $Rev: 7345 $)
+% Job saved on 11-Jul-2020 11:12:33 by cfg_util (rev $Rev: 7345 $)
 % spm SPM - SPM12 (7487)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.spm.spatial.coreg.estwrite.ref = '<UNDEFINED>';
-matlabbatch{1}.spm.spatial.coreg.estwrite.source = '<UNDEFINED>';
-matlabbatch{1}.spm.spatial.coreg.estwrite.other = {''};
-matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
-matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
-matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
-matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
-matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
-matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
-matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
-matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'c';
+matlabbatch{1}.spm.spatial.coreg.estimate.ref = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.coreg.estimate.source = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.coreg.estimate.other = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
