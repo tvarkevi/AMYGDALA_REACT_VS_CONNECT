ae02ba8efa1459d9362636ef2b7b5bc94c266a68
Tim Varkevisser <61967027+tvarkevi@users.noreply.github.com>
2020-11-30 10:45:39 +0100

    Update SliceTimingCorrection_job.m

--- SliceTimingCorrection_job.m
+++ SliceTimingCorrection_job.m
@@ -1,12 +1,1 @@
-%-----------------------------------------------------------------------
-% Job saved on 20-Feb-2020 13:28:57 by cfg_util (rev $Rev: 7345 $)
-% spm SPM - SPM12 (7487)
-% cfg_basicio BasicIO - Unknown
-%-----------------------------------------------------------------------
-matlabbatch{1}.spm.temporal.st.scans = {'<UNDEFINED>'};
-matlabbatch{1}.spm.temporal.st.nslices = 30;
-matlabbatch{1}.spm.temporal.st.tr = 1.6;
-matlabbatch{1}.spm.temporal.st.ta = 1.54666666666667;
-matlabbatch{1}.spm.temporal.st.so = [1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30];
-matlabbatch{1}.spm.temporal.st.refslice = 15;
-matlabbatch{1}.spm.temporal.st.prefix = 'a';
+
