2c52559a1e32e7207e9bf6b513fd0ff5753eacea
Tim Varkevisser <61967027+tvarkevi@users.noreply.github.com>
2020-11-30 11:26:52 +0100

    Create CoregisterGlobalMask_job.m

---
+++ CoregisterGlobalMask_job.m
@@ -0,0 +1,11 @@
+%-----------------------------------------------------------------------
+% Job saved on 14-Jul-2020 09:26:11 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.write.ref = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.coreg.write.source = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.coreg.write.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.coreg.write.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.coreg.write.roptions.mask = 0;
+matlabbatch{1}.spm.spatial.coreg.write.roptions.prefix = 'r';
