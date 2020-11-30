168a9d71ad24f60d2b2797769ba7a25b36dda94f
Tim Varkevisser <61967027+tvarkevi@users.noreply.github.com>
2020-11-30 10:53:44 +0100

    Create start_up.m

---
+++ start_up.m
@@ -0,0 +1,14 @@
+function start_up(input_workdir)
+
+% Add auxilliary programs to path.
+%
+% Input argument(input_workdir): Specify the working directory, the
+% subdirectory 'Programs' of which, contains the necessary auxilliary 
+% programs.
+% Subfunctions: -
+
+% ----- Add programs to path ----- %
+addpath([input_workdir '\Programs\spm12']);
+addpath([input_workdir '\Programs\r2agui_v27']);
+addpath(genpath([input_workdir '\Programs\hiro3']));
+addpath(genpath([input_workdir '\Programs\DPABI_V4.2\DPABI_V4.2_190919']));
